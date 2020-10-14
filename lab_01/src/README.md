e4ca78de08a1e4b9b6c4f19f579c5c3f8c1233a4
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2020-10-14 22:00:55 +0300

    Done for sure

--- lab_01/src/README.md
+++ lab_01/src/README.md
@@ -1,2 +1,5 @@
 # Инструкция к запуску демо
-...
+    python <name>_demo -i <path/to/image>
+или
+
+    python <name>_demo --image <path/to/image>
--- lab_01/src/processing/convert_to_grayscale.py
+++ lab_01/src/processing/convert_to_grayscale.py
@@ -6,4 +6,4 @@ def BGR2GRAY(image):
     return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 def bgr2gray(image):
-    return np.uint8(np.dot(image, [0.07, 0.72, 0.21])
+    return np.uint8(np.dot(image, [0.07, 0.72, 0.21]))
