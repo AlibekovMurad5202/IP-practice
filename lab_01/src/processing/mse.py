c683fa68fefc66402a8fc5cbd2036efe721b943e
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2020-10-14 19:30:26 +0300

    Update mse.py

--- lab_01/src/processing/mse.py
+++ lab_01/src/processing/mse.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def mse(img1, img2):
     epsilon = 1e-10
-    error = np.mean( (np.float32(cv.absdiff(img1, img2))) ** 2)
+    error = np.mean((np.float32(cv.absdiff(img1, img2))) ** 2)
     if error < epsilon:
         return 0
     else:
