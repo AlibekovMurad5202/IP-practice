b4096cf19dbad151e7206ea3cac57def15e087f6
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2020-11-03 17:15:45 +0300

    add random_normal

--- lab_02/src/processing/utils.py
+++ lab_02/src/processing/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+import math
 
 def clamp_bgr_pixel(colors, value):
     b_result = max(min(colors[0] + value, 255), 0)
@@ -21,3 +22,26 @@ def PSNR(img1, img2):
    else:
         psnr = 10.0 * np.log10((255.0 * 255.0) / mse)
         return psnr
+
+def random_normal(mean, var, shape):
+    gaussian = np.zeros(shape=shape, dtype='int16')
+    gaussian = gaussian.reshape(-1)
+    odd = False
+    rng = np.random.RandomState()
+    if len(gaussian)%2 == 1:
+        odd = True
+    half = len(gaussian) // 2
+
+    u1s, u2s = rng.uniform(size=half + 1), rng.uniform(size=half + 1)
+    ss = -np.log(u1s)
+    thetas = 2*math.pi*u2s
+    rs = np.sqrt(2*ss) * var
+    xs, ys = rs*np.cos(thetas) + mean, rs*np.sin(thetas) + mean
+
+    for i in range(0, half):
+        gaussian[2*i] = xs[i]
+        gaussian[2*i + 1] = ys[i]
+    if odd:
+        gaussian[-1] = xs[-1]
+    gaussian = gaussian.reshape(shape)
+    return gaussian
